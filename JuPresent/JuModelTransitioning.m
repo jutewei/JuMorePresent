f593c117ab06f6a298f7f70176d4a35e299e04fd
Juvid <jutewei@qq.com>
2018-04-23 11:29:19 +0800

    代码优化

--- JuPresent/JuModelTransitioning.m
+++ JuPresent/JuModelTransitioning.m
@@ -27,45 +27,6 @@
     }else{
         [self juDismissAnimation:transitionContext];
     }
-   /* UIView *containerView = [transitionContext containerView];
-    UIViewController *fromVC = [transitionContext  viewControllerForKey:UITransitionContextFromViewControllerKey];
-    UIViewController *toVC   = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
-    UIView *toView = [transitionContext viewForKey:UITransitionContextToViewKey];
-
-    CGRect toViewFinalFrame = [transitionContext finalFrameForViewController:toVC];
-
-    if (toView) {
-        [containerView addSubview:toView];
-        toView.frame = toViewFinalFrame;
-    }
-    if (self.isPresented) {
-        toVC.view.alpha=0;
-    }
-    // Animate using the animator's own duration value.
-    [UIView animateWithDuration:[self transitionDuration:transitionContext]
-                     animations:^{
-                         if (self.isPresented) {
-                             // Move the presented view into position.
-                             toView.alpha=1;
-                         }
-                         else {
-                             // Move the dismissed view offscreen.
-                             fromVC.view.alpha=0;
-                         }
-                     }
-                     completion:^(BOOL finished){
-                         BOOL success = ![transitionContext transitionWasCancelled];
-
-                         // After a failed presentation or successful dismissal, remove the view.
-                         if ((self.isPresented && !success) || (!self.isPresented && success)) {
-                             [toView removeFromSuperview];
-                         }
-
-                         // Notify UIKit that the transition has finished
-                         [transitionContext completeTransition:success];
-                     }];*/
-
-
 }
 //实现present动画逻辑代码
 - (void)juPresentAnimation:(id<UIViewControllerContextTransitioning>)transitionContext{
@@ -123,7 +84,6 @@
         }else{
             [transitionContext completeTransition:YES];
             toVC.view.hidden = NO;
-//            [containerView addSubview:toVC.view];
             //如果成功了，我们需要标记成功，同时让vc1显示出来，然后移除截图视图，
             [tempView removeFromSuperview];
         }
